f331d932fdfa6da304c2a0649605f764a7925aed
Brian Rogers <j.brian.rogers@gmail.com>
2022-01-19 13:29:54 -0800

    allow unlimited accounts and update the version string

--- scormcloud/scormcloud.php
+++ scormcloud/scormcloud.php
@@ -4,7 +4,7 @@ Plugin Name: SCORM Cloud For WordPress
 Plugin URI: http://scorm.com/wordpress
 Description: Tap the power of SCORM to deliver and track training right from your WordPress-powered site. Just add the SCORM Cloud widget to the sidebar or use the SCORM Cloud button to add a link directly in a post or page.
 Author: Rustici Software
-Version: 2.0.2
+Version: 2.0.3
 Author URI: http://www.scorm.com
  */
 
